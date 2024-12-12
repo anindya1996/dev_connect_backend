ef356be9c3e2df1b48706d116824586f7d8951f1
Anindya Sundar Mukherjee <anindya.mukherjee26@gmail.com>
2024-12-12 19:36:41 +0530

    schema validation,data sanitization etc

--- src/app.js
+++ src/app.js
@@ -5,19 +5,91 @@ config();
 const User = require("./models/user.js");
 const app = express();
 
+app.use(express.json());
+
+//Get user by email-/getUser
+app.get("/getUser", async (req, res) => {
+  const userEmail = req.body.email;
+  try {
+    const users = await User.findOne({ email: userEmail });
+    if (!users) {
+      res.status(404).send(`User not found`);
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(400).send(`Something went wrong,  ${err.message}`);
+  }
+});
+
+//Feed API-GET/feed- get all the users from the database
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send(`Something went wrong,  ${err.message}`);
+  }
+});
+
+//Add new user- /signup
 app.post("/signup", async (req, res, next) => {
-  //Creating a new instance of user model
-  const user = new User({
-    firstName: "Mayank",
-    lastName: "Keshri",
-    email: "mayank12@gmail.com",
-    password: "Mayank@123",
-  });
+  // Creating a new instance of user model
+  const user = new User(req.body);
+
   try {
     await user.save();
     res.send("User added successfully!!");
   } catch (err) {
     res.status(400).send(`Some error occured: ${err.message}`);
+  }
+});
+
+//Update an user- /update
+app.patch("/update/:userId", async (req, res) => {
+  const userId = req.params?.userId;
+  const data = req.body;
+
+  try {
+    const ALLOWED_UPDATES = [
+      "userId",
+      "photoUrl",
+      "about",
+      "skills",
+      "password",
+      "lastName",
+    ];
+
+    const isUpdateAllowed = Object.keys(data).every((key) =>
+      ALLOWED_UPDATES.includes(key)
+    );
+
+    if (!isUpdateAllowed) {
+      throw new Error(`Update is not allowed`);
+    }
+
+    if (data.skills.length > 10) {
+      throw new Error(`Skills cannot be more than 10`);
+    }
+
+    await User.findByIdAndUpdate({ _id: userId }, data, {
+      runValidators: true,
+    });
+    res.send(`User updated successfully`);
+  } catch (err) {
+    res.status(400).send(`Update failed, ${err.message}`);
+  }
+});
+
+//Delete user by id- /delete
+app.delete("/delete", async (req, res) => {
+  const userId = req.body.userId;
+
+  try {
+    await User.findByIdAndDelete(userId);
+    res.send(`User deleted successfully`);
+  } catch (err) {
+    res.status(400).send(`Something went wrong  ${err.message}`);
   }
 });
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,26 +1,75 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 //Creating USer Schema
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 4,
+      maxLength: 20,
+    },
+    lastName: {
+      type: String,
+      maxLength: 20,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error(`Email is not valid: ${value}`);
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: 8,
+      maxLength: 16,
+      validate(value) {
+        if (!validator.isStrongPassword(value)) {
+          throw new Error(`Password is not strong, ${value}`);
+        }
+      },
+    },
+    age: {
+      type: Number,
+      min: 18,
+      required: true,
+    },
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error(`Gender is not valid!!`);
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+      default:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRm-TruksPXPI5imDL_kfzEfFiAZwg5AzHtWg&s",
+      validate(value) {
+        if (!validator.isURL(value)) {
+          throw new Error(`Invalid photo URL: ${value}`);
+        }
+      },
+    },
+    about: {
+      type: String,
+      default: "I am a developer",
+    },
+    skills: {
+      type: [String],
+    },
   },
-  lastName: {
-    type: String,
-  },
-  email: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-});
+  { timestamps: true }
+);
 
 //Creating User Model
 const User = mongoose.model("User", userSchema);
